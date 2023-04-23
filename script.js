111c930e230fd8eb2f482b4b2d935b09f4d9f343
JCionx <jcionx0@gmail.com>
2023-04-23 18:21:51 +0100

    finished styling

---
+++ script.js
@@ -0,0 +1,64 @@
+function resize() {
+    var desktopView = document.getElementById('desktopView');
+    if (window.matchMedia("(orientation: landscape)").matches) {
+        desktopView.src = window.location.href;
+        desktopView.style.display = "block";
+        desktopView.style.height = "100vh";
+        desktopView.style.width = "50vw";
+        desktopView.style.overflow = "visible";
+        desktopView.scrollIntoView({behavior: "instant", block: "start"});
+    
+        const bodyWidth = document.body.clientWidth;
+        const iframeWidth = desktopView.offsetWidth;
+    
+        desktopView.style.marginLeft = `${(bodyWidth - iframeWidth) / 2}px`;
+        document.body.style.overflowY = "hidden";
+
+        document.body.style.background = "url(images/desktopBg.png) repeat";
+    } else {
+        desktopView.style.display = "none";
+        document.body.style.overflowY = "visible";
+        document.body.style.background = "white";
+    }
+}
+
+function removePlastic() {
+    let plastic = document.getElementById("plastic");
+    let opacity = 1;
+
+    let fadeOut = setInterval(function() {
+    if (opacity > 0) {
+        opacity -= 0.3;
+        plastic.style.opacity = opacity;
+    } else {
+        clearInterval(fadeOut);
+        plastic.style.display = "none";
+    }
+    }, 100);
+}
+
+function removeGlass() {
+    let glass = document.getElementById("glass");
+    let opacity = 1;
+
+    let fadeOut = setInterval(function() {
+    if (opacity > 0) {
+        opacity -= 0.3;
+        glass.style.opacity = opacity;
+    } else {
+        clearInterval(fadeOut);
+        glass.style.display = "none";
+    }
+    }, 100);
+}
+
+document.getElementById("bulb_off").style.display = "none";
+function bulb() {
+    document.getElementById("bulb_off").style.display = "block";
+    document.getElementById("bulb_on").style.display = "none";
+}
+
+window.addEventListener("resize", () => {
+    resize();
+});
+resize();
